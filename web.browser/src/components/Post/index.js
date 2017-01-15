7238e8542cbfea850a7174be821cc40508d55bdb
justinl-y <j@solaris.org.uk>
2017-01-14 21:28:17 -0800

    Post styles updated

--- web.browser/src/components/Post/index.js
+++ web.browser/src/components/Post/index.js
@@ -5,21 +5,24 @@ import Chip from 'material-ui/Chip';
 import styles from './styles.css';
 
 const Post = ({ title, link, description, vote, updateVote, categories }) => (
-  <li className={ styles[ 'post' ] }>
-    <Card>
-      <CardHeader>
+  <li className={ styles[ 'post-list' ] }>
+    <Card className={ styles[ 'post-item' ] }>
+      <CardHeader className={ styles[ 'post-header' ] }>
         <h2><a href={ link } target="_blank">{ title }</a></h2>
       </CardHeader>
       <CardText>
         <p>{ description }</p>
       </CardText>
-      <div className={ styles['post-buttons'] }>
-        <FlatButton onClick={ updateVote } className={ styles['vote-button'] }>
+      <div className={ styles[ 'post-buttons' ] }>
+        <FlatButton className={ styles[ 'post-vote-button' ] } onClick={ updateVote }>
           Vote { vote }
         </FlatButton>
-        <div className={ styles['category-chips'] }>
+        <div className={ styles['post-categories'] }>
           {
-            categories.map(( e, i ) => ( <Chip key={ i + e }>{ e }</Chip> ))
+            categories.map(( e, i ) => ( <Chip 
+                                            className={ styles[ 'post-chip' ] }
+                                            key={ i + e }>{ e } 
+                                          </Chip> ))
           }
         </div>
       </div>
