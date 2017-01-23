7f475c7fff8e01d0c5549e29ab3217116ce91cf0
justinl-y <j@solaris.org.uk>
2017-01-22 18:35:23 -0800

    Category reducer added

---
+++ web.browser/src/containers/Categories/actions.js
@@ -0,0 +1,9 @@
+const categoriesInitialState = { categories: undefined };
+
+export default (state = categoriesInitialState, action) => {
+  // console.log(state);
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
